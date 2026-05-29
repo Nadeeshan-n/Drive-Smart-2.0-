cac81d4b1ee3ddfd89cc4c172174e6d45b05b9bc
naeeshan <nadeeshannadeera14@gmail.com>
2026-05-29 20:12:28 +0530

    updated main window

--- Drive Smart 2.0/App.xaml.cs
+++ Drive Smart 2.0/App.xaml.cs
@@ -14,8 +14,8 @@ namespace Drive_Smart_2._0
         {
             base.OnStartup(e);
 
-            LoginWindow login = new LoginWindow();
-            login.Show();
+            //LoginWindow login = new LoginWindow();
+            //login.Show();
         }
     }
 
