36c37350ac34ca3adb31517332a40d2f45471023
Amshka <146643116+Amshka@users.noreply.github.com>
2026-06-03 01:20:52 +0530

    some changes about comments

--- Drive Smart 2.0/App.xaml.cs
+++ Drive Smart 2.0/App.xaml.cs
@@ -17,9 +17,20 @@ namespace Drive_Smart_2._0
             var dash = new Window1();
             dash.Show();
 
-            // Amishka's code dont put your a@# here
-            VehicleView_V2 window = new VehicleView_V2();
-            window.Show();
+
+            //---------------------------------------------------------
+            // Amishka's code dont put your a@# here [starts here]   |
+            //---------------------------------------------------------
+
+
+            /*VehicleView_V2 window = new VehicleView_V2();
+            window.Show();*/
+
+
+            //---------------------------------------------------------
+            // End of Amishka's Code                                 |
+            //---------------------------------------------------------
+
 
         }
     }
