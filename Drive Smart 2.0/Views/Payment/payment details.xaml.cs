933ebbedf372a850d022cc4532639be944bfe012
lap.lk <lap.lk@ISURU>
2026-06-03 19:21:02 +0530

    first commit

---
+++ Drive Smart 2.0/Views/Payment/payment details.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Drive_Smart_2._0.Views.Payment
+{
+    /// <summary>
+    /// Interaction logic for payment_details.xaml
+    /// </summary>
+    public partial class payment_details : Window
+    {
+        public payment_details()
+        {
+            InitializeComponent();
+        }
+    }
+}
