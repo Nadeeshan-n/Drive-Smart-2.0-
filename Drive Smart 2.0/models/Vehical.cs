4c185e123bf743961ea575a8241bb8119bfd5897
yasirud135 <dilshanya5@gmail.com>
2026-06-03 19:20:57 +0530

    table working

--- Drive Smart 2.0/models/Vehical.cs
+++ Drive Smart 2.0/models/Vehical.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Drive_Smart_2._0.models
 {
-    class Vehical
+    public class Vehicle
     {
+        public int Id { get; set; }
+        public string Brand { get; set; } = "";
+        public string Model { get; set; } = "";
+        public string PlateNumber { get; set; } = "";
+        public int Year { get; set; }
+        public string Color { get; set; } = "";
+        public double DailyRate { get; set; }
+        public string Status { get; set; } = "";
     }
 }
