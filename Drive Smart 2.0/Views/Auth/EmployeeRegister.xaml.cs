8e6463b50eec8faaba1ebb769556e04569ec6328
naeeshan <nadeeshannadeera14@gmail.com>
2026-06-03 19:35:41 +0530

    updated the register form

--- Drive Smart 2.0/Views/Auth/EmployeeRegister.xaml.cs
+++ Drive Smart 2.0/Views/Auth/EmployeeRegister.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace Drive_Smart_2._0.Views.Auth
 {
@@ -22,6 +23,40 @@ namespace Drive_Smart_2._0.Views.Auth
         public EmployeeRegister()
         {
             InitializeComponent();
+        }
+
+        private void EmployeeID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NIC_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void EmailAddress_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeID.Clear();
+            FullName.Clear();
+            NIC.Clear();
+            Gender.SelectedIndex = 0;
+            EmailAddress.Clear();
+            PhoneNumber.Clear();
+            DateOfBirth.SelectedDate = null;
+            Posision_Copy1.Clear();
+            JoiningDate.SelectedDate = null;
+            Address.Clear();
+        }
+
+        private void Employee_Register_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
