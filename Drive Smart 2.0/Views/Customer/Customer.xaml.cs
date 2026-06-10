d6386211fb03ade7b202c923cb035245462c1b92
M.p.s.m de Silva <salisadesilva@gmail.com>
2026-06-10 10:30:11 +0530

    custermer deatils

---
+++ Drive Smart 2.0/Views/Customer/Customer.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Drive_Smart_2._0.Views.Customer
+{
+    /// <summary>
+    /// Interaction logic for Customer.xaml
+    /// </summary>
+    public partial class Customer : Window
+    {
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_4(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_5(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_6(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_7(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string ph = phonenumber.Text;
+            if (ph.Length == 10)
+
+            {
+                MessageBox.Show("Valid phone number");
+            }
+            else
+            {
+                MessageBox.Show("Invalid phone number");
+            }
+            string idnumber = identity.Text;
+            if (idnumber.Length == 12)
+            {
+                MessageBox.Show("Valid ID number");
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID number");
+            }
+
+
+        }
+
+        private void TextBox_TextChanged_8(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_9(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_10(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_11(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
